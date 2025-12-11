122ecae4a844ec6f858d59ae1c72f0ddc3e11a7e
smuhriddin2006 <smuhriddin008@gmail.com>
2025-12-11 20:01:38 +0500

    Exam

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// Task 7 is done !
+// int SumOfDigits(int n)
+// {
+//     int i = n;
+//     int sum = 0;
+//     while(i > 0)
+//     {
+//         int digit = i % 10;
+//         i /= 10;
+//         sum += digit;
+//     }
+//     return sum;
+// }
+// int res = SumOfDigits(1234);
+// System.Console.WriteLine($"The sum of the digits of the number {res} ");
+/*-----------------------------------------*/
+// Task 10 is done
+// int n1 = int.Parse(Console.ReadLine());
+// int n2 = int.Parse(Console.ReadLine());
+// for(int i = n1; i <= n2; i++)
+// {
+//    if(i % 2 == 0)
+//     {
+//         System.Console.WriteLine(i);
+//     }
+// }
+// for(int i = n1; i <= n2; i++)
+// {
+//    if(i % 2 != 0)
+//     {
+//         System.Console.WriteLine(i);
+//     }
+// }
+
+
+// Task 5 is done !
+// int n = int.Parse(Console.ReadLine());
+// int[] arr = new int[n];
+// for(int i = 0; i < arr.Length; i++)
+// {
+//     arr[i] = int.Parse(Console.ReadLine());
+// }
+// int n2 = int.Parse(Console.ReadLine());
+// int cnt = 0;
+// for(int i = 0; i < arr.Length; i++)
+// {
+//     if(n2 == arr[i])
+//     {
+//         cnt++;
+//     }
+// }
+// System.Console.WriteLine(cnt);
+
+
+//Task 4
+// int n = int.Parse(Console.ReadLine());
+// int[] arr = new int[n];
+// for(int i = 0; i < arr.Length; i++)
+// {
+//     arr[i] = int.Parse(Console.ReadLine());
+// }
+// for(int i = 0; i < arr.Length; i++)
+// {
+//         if(arr[i] > 0)
+//     {
+        
+//         arr[i] = 0;
+        
+//     }
+//     System.Console.WriteLine(arr[i]);
+// }
+
+
